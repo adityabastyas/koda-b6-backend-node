0fa9f66b59963a2397f003949733472074dd2fba
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-01 08:53:27 +0700

    feat: add productRouter get id in routes

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -4,5 +4,6 @@ import * as pc from "../controllers/product.controller.js"
 const productRouter = Router()
 
 productRouter.get("",pc.GetAllProduct)
+productRouter.get("/:id", pc.GetProductId)
 
 export default productRouter
