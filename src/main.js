363f637fe87aee3a8c4e6730ed2dd1742098252b
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-11 09:11:58 +0700

    feat: add kategory routes to main application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import express from "express"
 import usersRouter from "./routes/users.router.js"
 import productRouter from "./routes/product.router.js"
+import kategoryRouter from "./routes/kategory.router.js"
 
 const app = express()
 const PORT = process.env.PORT || 8888
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8888
 app.use(express.json())
 app.use("/", usersRouter)
 app.use("/product", productRouter)
+app.use("/kategory", kategoryRouter)
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}`)
