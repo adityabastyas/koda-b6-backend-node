42b86f9510a385177c5b80f2d51826bc4cbde0cc
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-11 09:11:22 +0700

    feat: add promo routes with CRUD functionalities to product router

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import * as pc from "../controllers/product.controller.js"
+import * as promo from "../controllers/promo.controller.js"
+
 
 const productRouter = Router()
 
 productRouter.get("",pc.GetAllProduct)
+productRouter.post("", pc.CreateProduct)
+productRouter.patch("/:id", pc.UpdateProduct)
+productRouter.delete("/:id", pc.DeleteProduct)
+
+productRouter.get("/promo", promo.GetAllPromo)
+productRouter.get("/promo/:id", promo.GetPromoId)
+productRouter.post("/promo", promo.CreatePromo)
+productRouter.patch("/promo/:id", promo.UpdatePromo)
+productRouter.delete("/promo/:id", promo.DeletePromo)
+
 productRouter.get("/:id", pc.GetProductId)
 
 export default productRouter
