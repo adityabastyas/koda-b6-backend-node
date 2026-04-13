dab72edb843b6c9456a0f9382f6ef1ba13ccc833
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-13 15:36:51 +0700

    add function createcart and deletecart with query sql

--- src/models/cart.models.js
+++ src/models/cart.models.js
@@ -13,7 +13,7 @@ import { db } from "../lib/db.js"
  * 
  */
 export async function GetAllCart() {
-  const sql = `SELECT cart_id, user_id FROM carts`
+  const sql = `SELECT cart_id, user_id FROM cart`
 
   const result = await db().query(sql)
   return result.rows ?? null
@@ -25,7 +25,7 @@ export async function GetAllCart() {
  * @returns {Promise<Cart[]>}
  */
 export async function GetCartId(cartId){
-  const sql = `SELECT cart_id, user_id FROM carts WHERE cart_id = $1`
+  const sql = `SELECT cart_id, user_id FROM cart WHERE cart_id = $1`
   const values = [cartId]
 
   const result = await db().query(sql, values)
@@ -38,10 +38,38 @@ export async function GetCartId(cartId){
  * @returns {Promise<Cart[]>}
  */
 export async function GetCartUserId(userId) {
-  const sql = `SELECT cart_id, user_id FROM cart WHERE user_id = $`
+  const sql = `SELECT cart_id, user_id FROM cart WHERE user_id = $1`
   const values = [userId]
 
   const result = await db().query(sql, values)
   return result.rows ?? null
+  
+}
+
+/**
+ * create cart
+ * @param {number} userId 
+ * @returns {Promise<Cart>}
+ */
+export async function CreateCart(userId) {
+  const sql = `INSERT INTO cart (user_id) VALUES ($1) RETURNING cart_id, user_id`
+
+  const values = [userId]
+
+  const result = await db().query(sql, values)
+  return result.rows[0]
+  
+}
+
+/**
+ * delete cart
+ * @param {number} cartId 
+ * @returns {Promise<Cart|null>}
+ */
+export async function DeleteCart(cartId) {
+  const sql = `DELETE FROM cart WHERE cart_id = $1 RETURNING cart_id, user_id`
+
+  const result = await db().query(sql, [cartId])
+  return result.rows[0] ?? null
   
 }
