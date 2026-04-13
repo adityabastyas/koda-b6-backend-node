ef3123a2bdeb40e1add143bfe8c8029479bc72b7
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-13 16:34:10 +0700

    add function GetCartId with validation

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -3,14 +3,36 @@ import * as cm from "../models/cart.models.js"
 /**
  * GET ALL
  */
-
 export async function GetAllCart(req, res) {
-  const data = await cm.GetAllCart
+  const data = await cm.GetAllCart()
 
   if (!data) {
     return res.json({
       success : false,
       message : "gagal ambil cart",
+      result : null
+    })
+  }
+
+  return res.json({
+    success : true,
+    message : "berhasil ambil cart",
+    result : data
+  })
+  
+}
+
+/**
+ * get by id
+ */
+
+export async function GetCartId(req, res) {
+  const data = await cm.GetCartId(req.params.id)
+
+  if (!data || data.length < 1) {
+    return res.json({
+      success : false,
+      message : "cart tidak ditemukan",
       result : null
     })
   }
