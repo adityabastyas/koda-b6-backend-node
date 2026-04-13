0465c598ae8c7e81dd317a84dfd0c5406cb0694c
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-13 15:08:21 +0700

    feat: add documentation route to serve API documentation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import express from "express"
 import usersRouter from "./routes/users.router.js"
 import productRouter from "./routes/product.router.js"
 import kategoryRouter from "./routes/kategory.router.js"
+import docsRouter from "./routes/docs.js"
+
 
 const app = express()
 const PORT = process.env.PORT || 8888
@@ -10,6 +12,8 @@ app.use(express.json())
 app.use("/", usersRouter)
 app.use("/product", productRouter)
 app.use("/kategory", kategoryRouter)
+
+app.use("/docs", docsRouter)
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}`)
