0ee19d6b421f2af5f58b07c6cbdea4011d2ad6cb
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-13 15:08:10 +0700

    feat: add cart route to retrieve all cart items

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import * as pc from "../controllers/product.controller.js"
 import * as promo from "../controllers/promo.controller.js"
+import * as cart from "../controllers/cart.controller.js"
 
 
 const productRouter = Router()
 
+/**
+ * @swagger
+ * /product:
+ *  get:
+ *    summary: ambil semua product
+ *    responses:
+ *      200:
+ *        description: berhasil ambil product
+ */
 productRouter.get("",pc.GetAllProduct)
 productRouter.post("", pc.CreateProduct)
 productRouter.patch("/:id", pc.UpdateProduct)
@@ -16,6 +26,9 @@ productRouter.post("/promo", promo.CreatePromo)
 productRouter.patch("/promo/:id", promo.UpdatePromo)
 productRouter.delete("/promo/:id", promo.DeletePromo)
 
+productRouter.get("/cart", cart.GetAllCart)
+
 productRouter.get("/:id", pc.GetProductId)
+
 
 export default productRouter
