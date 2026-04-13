c1267cd468bf8082c8e653004fece4f3fce61208
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-13 17:22:34 +0700

    add cart routes for user and id retrieval, creation, and deletion with Swagger documentation

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -29,9 +29,76 @@ productRouter.delete("/promo/:id", promo.DeletePromo)
 
 // cart
 productRouter.get("/cart", cart.GetAllCart)
+
+/**
+ * @swagger
+ * /product/cart/user/{user_id}:
+ *   get:
+ *     summary: ambil cart berdasarkan user id
+ *     parameters:
+ *       - in: path
+ *         name: user_id
+ *         required: true
+ *         schema:
+ *           type: number
+ *     responses:
+ *       200:
+ *         description: berhasil ambil cart user
+ */
 productRouter.get("/cart/user/:user_id", cart.GetCartUserId)
+
+/**
+ * @swagger
+ * /product/cart/{id}:
+ *   get:
+ *     summary: ambil cart berdasarkan id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: number
+ *     responses:
+ *       200:
+ *         description: berhasil ambil cart
+ */
 productRouter.get("/cart/:id", cart.GetCartId)
+
+/**
+ * @swagger
+ * /product/cart:
+ *   post:
+ *     summary: buat cart
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: berhasil buat cart
+ */
 productRouter.post("/cart", cart.CreateCart)
+
+/**
+ * @swagger
+ * /product/cart/{id}:
+ *   delete:
+ *     summary: delete cart
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: number
+ *     responses:
+ *       200:
+ *         description: berhasil delete cart
+ */
 productRouter.delete("/cart/:id", cart.DeleteCart)
 
 productRouter.get("/:id", pc.GetProductId)
