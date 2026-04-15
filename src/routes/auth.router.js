236e7836692687789cf579dd77156fa89d68ab1c
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 17:03:38 +0700

    fix: update auth router to use auth.controller for registration and password functionalities

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import * as usersController from "../controllers/users.controller.js"
+import * as ac from "../controllers/auth.controller.js"
 
 const authRouter = Router()
 
-authRouter.post("/register", usersController.register)
+authRouter.post("/register", ac.register)
+authRouter.post("/login", ac.login)
 
-authRouter.post("/login", usersController.login)
+authRouter.post("/forgot-password", ac.requestForgotPassword)
+authRouter.patch("/forgot-password", ac.resetPassword)
 
 export default authRouter
