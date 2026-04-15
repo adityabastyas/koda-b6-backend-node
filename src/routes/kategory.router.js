70f6a69cba447e670008cd530bf5e9db41a3f3e5
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 15:07:16 +0700

    feat: update kategory router to improve endpoint consistency and add authentication middleware

--- src/routes/kategory.router.js
+++ src/routes/kategory.router.js
@@ -1,12 +1,15 @@
 import { Router } from "express"
 import * as kc from "../controllers/kategory.controller.js"
+import auth from "../middlewares/auth.middleware.js"
+import rbac from "../middlewares/rbac.middleware.js"
 
 const kategoryRouter = Router()
 
-kategoryRouter.get("", kc.GetAllKategory)
-kategoryRouter.get("/:id", kc.GetKategoryId)
-kategoryRouter.post("", kc.CreateKategory)
-kategoryRouter.patch("/:id", kc.UpdateKategory)
-kategoryRouter.delete("/:id", kc.DeleteKategory)
+kategoryRouter.get("/", kc.getAll)
+kategoryRouter.get("/:id", kc.getById)
+
+kategoryRouter.post("/", auth, rbac("admin"), kc.create)
+kategoryRouter.patch("/:id", auth, rbac("admin"), kc.update)
+kategoryRouter.delete("/:id", auth, rbac("admin"), kc.remove)
 
 export default kategoryRouter
