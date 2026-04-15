2d41583ee629f86c4cf4e96a3c0f0f0c983d6908
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-16 04:47:27 +0700

    feat: remove user registration and login functions from users controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,6 @@
 import { constants } from "node:http2"
 import * as userModel from "../models/users.models.js"
-import { GenerateHash, VerifyHash } from "../lib/hash.js"
-import { GenerateToken } from "../lib/jwt.js"
+
 
 /**
  * @typedef {import('express').Request} Request
@@ -30,100 +29,6 @@ export async function getAll(req, res) {
   }
 }
 
-/**
- * Register user
- * @param {Request} req
- * @param {Response} res
- * @returns {Promise<void>}
- */
-export async function register(req, res) {
-  try {
-    const { full_name, email, password } = req.body
-
-    if (!email || !password) {
-      return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
-        success: false,
-        message: "email & password tidak boleh kosong"
-      })
-    }
-
-    const existingUser = await userModel.findByEmail(email)
-    if (existingUser) {
-      return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
-        success: false,
-        message: "email sudah terdaftar"
-      })
-    }
-
-    const hashedPassword = await GenerateHash(password)
-
-    await userModel.save({
-      full_name,
-      email,
-      password: hashedPassword
-    })
-
-    res.status(constants.HTTP_STATUS_OK).json({
-      success: true,
-      message: "register success"
-    })
-  } catch (err) {
-    res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: err.message
-    })
-  }
-}
-
-/**
- * Login user
- * @param {Request} req
- * @param {Response} res
- * @returns {Promise<void>}
- */
-export async function login(req, res) {
-  try {
-    const { email, password } = req.body
-
-    const user = await userModel.findByEmail(email)
-
-    if (!user) {
-      return res.status(constants.HTTP_STATUS_UNAUTHORIZED).json({
-        success: false,
-        message: "email atau password salah"
-      })
-    }
-
-    console.log("HASH DB:", user.password)
-
-    const valid = await VerifyHash(password, user.password)
-
-    if (!valid) {
-      return res.status(constants.HTTP_STATUS_UNAUTHORIZED).json({
-        success: false,
-        message: "invalid email or password"
-      })
-    }
-
-    const token = GenerateToken(user.user_id, user.role)
-
-    const { password: _, ...safeUser } = user
-
-    res.status(constants.HTTP_STATUS_OK).json({
-      success: true,
-      message: "login success",
-      result: {
-        user: safeUser,
-        token
-      }
-    })
-  } catch (err) {
-    res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: err.message
-    })
-  }
-}
 
 /**
  * Update profile
