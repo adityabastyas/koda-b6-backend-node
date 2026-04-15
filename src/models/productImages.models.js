fc232f5b23da18bf08ea1948bcf19808e8419214
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 17:12:45 +0700

    feat: add create and remove functions for product images

--- src/models/productImages.models.js
+++ src/models/productImages.models.js
@@ -22,3 +22,37 @@ export async function getByProductId(productId) {
   const result = await pool.query(sql, [productId])
   return result.rows
 }
+
+/**
+ * Create image
+ * @param {Object} data
+ * @returns {Promise<ProductImage>}
+ */
+export async function create(data) {
+  const { product_id, path } = data
+
+  const sql = `
+    INSERT INTO product_images (product_id, path)
+    VALUES ($1, $2)
+    RETURNING product_images_id, product_id, path
+  `
+
+  const result = await pool.query(sql, [product_id, path])
+  return result.rows[0]
+}
+
+/**
+ * Delete image
+ * @param {number} id
+ * @returns {Promise<ProductImage|null>}
+ */
+export async function remove(id) {
+  const sql = `
+    DELETE FROM product_images
+    WHERE product_images_id = $1
+    RETURNING product_images_id, product_id, path
+  `
+
+  const result = await pool.query(sql, [id])
+  return result.rows[0] || null
+}
