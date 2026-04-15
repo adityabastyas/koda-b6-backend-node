5a826fbf5eeabf50237035d874940d84fc736bf2
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 15:18:13 +0700

    feat: refactor cart model functions for consistency and improved database handling

--- src/models/cart.models.js
+++ src/models/cart.models.js
@@ -1,7 +1,7 @@
-import { db } from "../lib/db.js"
+import pool from "../lib/db.js"
 
 /**
- * @typedef Cart
+ * @typedef {Object} Cart
  * @property {number} cart_id
  * @property {number} user_id
  */
@@ -12,64 +12,38 @@ import { db } from "../lib/db.js"
  * @returns {Promise<Cart[]>}
  * 
  */
-export async function GetAllCart() {
+export async function getAll() {
   const sql = `SELECT cart_id, user_id FROM cart`
 
-  const result = await db().query(sql)
-  return result.rows ?? null
-}
-
-/**
- * get cart id
- * @param {number} cartId 
- * @returns {Promise<Cart[]>}
- */
-export async function GetCartId(cartId){
-  const sql = `SELECT cart_id, user_id FROM cart WHERE cart_id = $1`
-  const values = [cartId]
-
-  const result = await db().query(sql, values)
-  return result.rows ?? null
+  const result = await pool.query(sql)
+  return result.rows
 }
 
 /**
  * get cart by user id
  * @param {number} userId 
- * @returns {Promise<Cart[]>}
+ * @returns {Promise<Cart|null>}
  */
-export async function GetCartUserId(userId) {
+export async function getByUserId(userId){
   const sql = `SELECT cart_id, user_id FROM cart WHERE user_id = $1`
   const values = [userId]
 
-  const result = await db().query(sql, values)
-  return result.rows ?? null
-  
+  const result = await pool.query(sql, values)
+  return result.rows[0] || null
 }
+
 
 /**
  * create cart
  * @param {number} userId 
  * @returns {Promise<Cart>}
  */
-export async function CreateCart(userId) {
+export async function create(userId) {
   const sql = `INSERT INTO cart (user_id) VALUES ($1) RETURNING cart_id, user_id`
 
   const values = [userId]
 
-  const result = await db().query(sql, values)
+  const result = await pool.query(sql, values)
   return result.rows[0]
-  
-}
-
-/**
- * delete cart
- * @param {number} cartId 
- * @returns {Promise<Cart|null>}
- */
-export async function DeleteCart(cartId) {
-  const sql = `DELETE FROM cart WHERE cart_id = $1 RETURNING cart_id, user_id`
-
-  const result = await db().query(sql, [cartId])
-  return result.rows[0] ?? null
   
 }
