204ea66ba2f4c5baad5a9bbe846edaed5cda1baa
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 17:39:13 +0700

    feat: add product variant model with getByProductId function

---
+++ src/models/productVariant.models.js
@@ -0,0 +1,23 @@
+import pool from "../lib/db.js"
+
+/**
+ * @typedef {Object} ProductVariant
+ * @property {number} variant_id
+ * @property {number} product_id
+ * @property {string} temperature
+ * @property {number} add_price
+ */
+
+/**
+ * Get by product id
+ */
+export async function getByProductId(productId) {
+  const sql = `
+    SELECT variant_id, product_id, temperature, add_price
+    FROM product_variant
+    WHERE product_id = $1
+  `
+
+  const result = await pool.query(sql, [productId])
+  return result.rows
+}
