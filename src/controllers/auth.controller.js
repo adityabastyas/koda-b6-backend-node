521b618274acca0e0b776f84ffd2952205f0026f
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 17:02:28 +0700

    feat: implement reset password functionality with validation and hashing

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import * as forgotModel from "../models/forgotPassword.models.js"
 import * as userModel from "../models/users.models.js"
 import { constants } from "node:http2"
-
+import { GenerateHash } from "../lib/hash.js"
 
 export async function requestForgotPassword(req, res) {
   try {
@@ -34,6 +34,60 @@ export async function requestForgotPassword(req, res) {
     return res.status(constants.HTTP_STATUS_OK).json({
       success: true,
       message: "code berhasil dikirim"
+    })
+  } catch (err) {
+    return res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: err.message
+    })
+  }
+}
+
+
+export async function resetPassword(req, res) {
+  try {
+    const { code, new_password } = req.body
+    const email = req.query.email
+
+    if (!email) {
+      return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "email tidak boleh kosong"
+      })
+    }
+
+    if (!code || !new_password || new_password.length < 6) {
+      return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "password minimal 6 karakter"
+      })
+    }
+
+    const data = await forgotModel.getByEmailAndCode(email, code)
+
+    if (!data) {
+      return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "code tidak valid atau sudah expired"
+      })
+    }
+
+    if (Date.now() - new Date(data.created_at) > 5 * 60 * 1000) {
+      return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+       success: false,
+       message: "OTP expired"
+     })
+    }
+
+    const hashed = await GenerateHash(new_password)
+
+    await userModel.updatePassword(email, hashed)
+
+    await forgotModel.removeByCode(code)
+
+    return res.status(constants.HTTP_STATUS_OK).json({
+      success: true,
+      message: "password berhasil direset"
     })
   } catch (err) {
     return res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
