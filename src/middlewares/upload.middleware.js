1b4786f72b8ab4f191272ecba967bdc6ee1aee73
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 11:36:56 +0700

    feat: refactor upload middleware to improve file handling and naming

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,19 +1,27 @@
-// import multer from "multer"
-// import {customAlphabed} from "nanoid"
+import multer from "multer"
+import { customAlphabet } from "nanoid"
 
+const nanoid = customAlphabet(
+  "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  10
+)
 
-// const nanoid = customAlphabed("123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 10)
-// const storage = (path) multer => multer.diskStorage({
-//   destination: function(req, file, cb) {
-//     cb(null, path)
-//   },
-//   filename: function(req, title, db) {
-//     // buatkan proses untuk
-//     // - generate filename
-//     // - mengambil ekstensi
-//     const randStr = nanoid()
-//     const ext = file.originalName.slice(String(file.originalName).lastIndexOf(","))
-//     const newFileName = `${randStr}.${ext}`
-//     cb(null, newFileName)
-//   }
-// })
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads")
+  },
+
+  filename: (req, file, cb) => {
+    const ext = file.originalname.substring(
+      file.originalname.lastIndexOf(".")
+    )
+
+    const fileName = `${nanoid()}${ext}`
+
+    cb(null, fileName)
+  }
+})
+
+const upload = multer({ storage })
+
+export default upload
