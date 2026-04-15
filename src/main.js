90ebea96c274f288d9a4a01899df00f8a8a57aa7
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 11:00:34 +0700

    feat: reorganize middleware imports and ensure auth router is included

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
+import "dotenv/config"
 import express from "express"
 import usersRouter from "./routes/users.router.js"
 import productRouter from "./routes/product.router.js"
 import kategoryRouter from "./routes/kategory.router.js"
 import docsRouter from "./routes/docs.js"
 import corsMiddleware from "./lib/cors.js"
-
+import authRouter from "./routes/auth.routes.js"
 
 const app = express()
 const PORT = process.env.PORT || 8888
 
 
-app.use(corsMiddleware)
 app.use(express.json())
+app.use(corsMiddleware)
 
 app.use("/uploads", express.static("uploads"))
 
+app.use("/auth", authRouter)
 app.use("/users", usersRouter)
 app.use("/products", productRouter)
 app.use("/kategorys", kategoryRouter)
