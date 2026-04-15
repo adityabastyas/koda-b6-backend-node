66f4717a4871436f03de3e507d88065788b1b15c
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 20:50:58 +0700

    feat: add GetById function to retrieve a transaction by ID

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -20,3 +20,32 @@ export async function GetAll(req, res) {
     })
   }
 }
+
+/**
+ * GET BY ID
+ */
+export async function GetById(req, res) {
+  try {
+    const id = parseInt(req.params.id)
+
+    if (isNaN(id)) {
+      return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "id harus angka"
+      })
+    }
+
+    const data = await tm.getById(id)
+
+    return res.status(constants.HTTP_STATUS_OK).json({
+      success: true,
+      message: "success",
+      result: data
+    })
+  } catch (err) {
+    return res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: err.message
+    })
+  }
+}
