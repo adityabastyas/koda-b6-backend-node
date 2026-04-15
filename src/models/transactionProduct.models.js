e4a6d0e0ca5ec37c2d409de8fab9dd553da08648
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 20:58:26 +0700

    feat: implement create and remove functions for transaction products

--- src/models/transactionProduct.models.js
+++ src/models/transactionProduct.models.js
@@ -13,3 +13,39 @@ export async function getByTransactionId(transactionId) {
   const result = await pool.query(sql, [transactionId])
   return result.rows
 }
+
+/**
+ * CREATE
+ */
+export async function create(transactionId, data) {
+  const {
+    product_id,
+    variant_id,
+    product_size_id,
+    quantity,
+    price_at_purchase
+  } = data
+
+  const sql = `
+    INSERT INTO transaction_product
+    (transaction_id, product_id, variant_id, product_size_id, quantity, price_at_purchase)
+    VALUES ($1,$2,$3,$4,$5,$6)
+  `
+
+  await pool.query(sql, [
+    transactionId,
+    product_id,
+    variant_id,
+    product_size_id,
+    quantity,
+    price_at_purchase
+  ])
+}
+
+/**
+ * DELETE
+ */
+export async function remove(id) {
+  const sql = `DELETE FROM transaction_product WHERE transaction_product_id = $1`
+  await pool.query(sql, [id])
+}
