1b8bc146c9344b9bdbe6dacb8fbe26a14400858c
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 19:14:51 +0700

    feat: add GetByUserId function to retrieve reviews by user ID

--- src/controllers/reviews.controller.js
+++ src/controllers/reviews.controller.js
@@ -42,3 +42,25 @@ export async function GetByProductId(req, res) {
     result: data
   })
 }
+
+/**
+ * GET BY USER ID
+ */
+export async function GetByUserId(req, res) {
+  const id = parseInt(req.params.user_id)
+
+  if (isNaN(id)) {
+    return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+      success: false,
+      message: "user id harus berupa angka"
+    })
+  }
+
+  const data = await rm.getByUserId(id)
+
+  return res.status(constants.HTTP_STATUS_OK).json({
+    success: true,
+    message: "success",
+    result: data
+  })
+}
