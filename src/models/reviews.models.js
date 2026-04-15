09a3bd5f31b436407d30b216fc105e3ac25646a2
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 19:13:19 +0700

    feat: add create and remove functions for review management

--- src/models/reviews.models.js
+++ src/models/reviews.models.js
@@ -74,3 +74,25 @@ export async function getByUserId(userId) {
   const result = await pool.query(sql, [userId])
   return result.rows
 }
+
+/**
+ * Create review
+ */
+export async function create(userId, data) {
+  const { product_id, message, rating } = data
+
+  const sql = `
+    INSERT INTO reviews (product_id, user_id, message, rating)
+    VALUES ($1, $2, $3, $4)
+  `
+
+  await pool.query(sql, [product_id, userId, message, rating])
+}
+
+/**
+ * Delete review
+ */
+export async function remove(id) {
+  const sql = `DELETE FROM reviews WHERE reviews_id = $1`
+  await pool.query(sql, [id])
+}
