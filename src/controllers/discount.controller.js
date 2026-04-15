8c9a5d4c479cb54fecd5065c5a16367925b73e5b
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 17:51:42 +0700

    feat: implement getById function in discount controller for retrieving discount by ID

--- src/controllers/discount.controller.js
+++ src/controllers/discount.controller.js
@@ -20,3 +20,39 @@ export async function getAll(req, res) {
     })
   }
 }
+
+/**
+ * GET BY ID
+ */
+export async function getById(req, res) {
+  try {
+    const id = parseInt(req.params.id)
+
+    if (isNaN(id)) {
+      return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "id harus berupa angka"
+      })
+    }
+
+    const data = await dm.getById(id)
+
+    if (!data) {
+      return res.status(constants.HTTP_STATUS_NOT_FOUND).json({
+        success: false,
+        message: "discount tidak ditemukan"
+      })
+    }
+
+    return res.status(constants.HTTP_STATUS_OK).json({
+      success: true,
+      message: "success",
+      result: data
+    })
+  } catch (err) {
+    return res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: err.message
+    })
+  }
+}
