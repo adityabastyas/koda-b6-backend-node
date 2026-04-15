98dd8469bba335d1d56ba21db8cb421573474ac7
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 17:29:55 +0700

    feat: add productSize model with function to retrieve sizes by product ID

---
+++ src/models/productSize.models.js
@@ -0,0 +1,23 @@
+import pool from "../lib/db.js"
+
+/**
+ * @typedef {Object} ProductSize
+ * @property {number} product_size_id
+ * @property {number} product_id
+ * @property {string} name
+ * @property {number} add_price
+ */
+
+/**
+ * Get by product id
+ */
+export async function getByProductId(productId) {
+  const sql = `
+    SELECT product_size_id, product_id, name, add_price
+    FROM product_size
+    WHERE product_id = $1
+  `
+
+  const result = await pool.query(sql, [productId])
+  return result.rows
+}
