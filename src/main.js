d92df5d5d7c8a11c02852fa221eb062a70c522ff
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 11:37:19 +0700

    feat: correct import path for auth router to maintain consistency

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import productRouter from "./routes/product.router.js"
 import kategoryRouter from "./routes/kategory.router.js"
 import docsRouter from "./routes/docs.js"
 import corsMiddleware from "./lib/cors.js"
-import authRouter from "./routes/auth.routes.js"
+import authRouter from "./routes/auth.router.js"
 
 const app = express()
 const PORT = process.env.PORT || 8888
