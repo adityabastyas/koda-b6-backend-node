96fda5412dd33e17c996ba6764e5a1383613718e
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-15 17:14:26 +0700

    feat: add productImages router to main application for image management

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import kategoryRouter from "./routes/kategory.router.js"
 import docsRouter from "./routes/docs.js"
 import corsMiddleware from "./lib/cors.js"
 import authRouter from "./routes/auth.router.js"
+import productImagesRouter from "./routes/productImages.router.js"
 
 const app = express()
 const PORT = process.env.PORT || 8888
@@ -17,10 +18,11 @@ app.use(corsMiddleware)
 app.use("/uploads", express.static("uploads"))
 
 app.use("/auth", authRouter)
-app.use("/users", authRouter)
+app.use("/users", usersRouter)
 app.use("/products", productRouter)
 app.use("/kategorys", kategoryRouter)
 app.use("/docs", docsRouter)
+app.use("/product-images", productImagesRouter)
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}`)
